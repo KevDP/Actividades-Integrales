5f1fdfe78d73b9a69d4e7f7330a529faed98252c
KevDP <58478385+KevDP@users.noreply.github.com>
2020-11-03 23:17:10 -0600

    Update main.cpp

--- Actividad Integral 3. Árboles/main.cpp
+++ Actividad Integral 3. Árboles/main.cpp
@@ -3,25 +3,38 @@
 #include "heap.h"
 #include <fstream>
 #include <sstream>
-#include <vector>
-#include "metodo.h"
-#include "datosCarta.h"
+
 
 using namespace std;
 
-template<class Carta>
-Heap<Carta> mi_arbol;
-template<class Carta>
-Metodo<Carta> mi_metodo;
+template <class T>
+bool leer(Heap<T> &mi_arbol, string nom_arch){
+  ifstream inFile(nom_arch);
+  if(!inFile){
+    cout<<"No es posible abrir el archivo"<<endl;
+    return false;
+  }
+  string val;
+  while(getline(inFile, val)){
+      if(val.size() > 0){
+        mi_arbol.push(val);
+		  }
+  }
+    cout<<"\n Datos obtenidos correctamente de archivo.txt\n"<<"\n";
+
+  inFile.close();
+  return true;
+}
 
 int main(){
-
-Heap<Carta> mi_arbol;
-Metodo<Carta> mi_metodo;
 int opcion;
+Heap<string> mi_arbol;
+bool archivo = leer(mi_arbol, "datos.csv");
+string datcarta;
 
 do {
-		cout << "1. Obtener datos\n";
+    cout << "Datos obtenidos del archivo"<<endl;
+		cout << "1. Desplegar datos\n";
     cout << "2. Agregar dato\n";
     cout << "3. Escribir datos en archivo\n";
 		cout << "4. Buscar dato\n";
@@ -29,10 +42,12 @@ do {
 		cin >> opcion;
 		cin.ignore();
     switch (opcion){
-      case 1: mi_metodo.leer(); break;
-      case 2: mi_metodo.agregar(); break;
-      case 3: mi_metodo.escribir(); break;
-      case 4: mi_metodo.buscarEnArbol(); break;
+      case 1: mi_arbol.toString(); break;
+      case 2:  break;
+      case 3: mi_arbol.escribir("resultado.csv"); break;
+      case 4: cout<<"Introduce el dato a verificar si está en el archivo"<<endl;
+              cin>>datcarta;
+              mi_arbol.find(datcarta); break;
       case 5 : cout << "Saliendo..."; return 0;
 			default: cout << "Opcion incorrecta\n"; break;
 		}
@@ -41,57 +56,3 @@ do {
 	return 0;
 }
 
-/*
-
-int main(int argc, char* argv[]) {
-	string in_ans = "", pre_ans = "";
-	SplayTree<int> my_splay;
-	bool found = 0;
-
-	my_splay.add(15);
-	in_ans =	"[15]";
-	cout << " 1 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
-	
-	my_splay.add(10);
-	my_splay.add(17);
-	
-	my_splay.add(7);
-	my_splay.add(13);
-	
-	my_splay.add(16);
-	in_ans =	"[7 10 13 15 16 17]";
-	pre_ans =	"[16 13 7 10 15 17]";
-	cout << " 2 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
-	cout << " 2 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
-
-	found = my_splay.find(15);
-	in_ans =	"[7 10 13 15 16 17]";
-	pre_ans =	"[15 13 7 10 16 17]";
-	cout << " 3 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
-	cout << " 3 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
-	cout << " 3 " <<	(found == 1 ? "success\n" : "fail\n");
-
-	found = my_splay.find(17);
-	in_ans =	"[7 10 13 15 16 17]";
-	pre_ans =	"[17 16 15 13 7 10]";
-	cout << " 4 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
-	cout << " 4 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
-	cout << " 4 " <<	(found == 1 ? "success\n" : "fail\n");
-
-	my_splay.remove(13);  //bottum up
-	in_ans =	"[7 15 16 17]";
-	pre_ans =	"[15 7 16 17]";
-	cout << " 5 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
-	cout << " 5 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
-	cout << " 5 " <<	(found == 1 ? "success\n" : "fail\n");
-
-	my_splay.remove(16);  //bottum up
-	in_ans =	"[7 15 17]";
-	pre_ans =	"[15 7 17]";
-	cout << " 6 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
-	cout << " 6 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
-	cout << " 6 " <<	(found == 1 ? "success\n" : "fail\n");
-	
-}
-
-*/
