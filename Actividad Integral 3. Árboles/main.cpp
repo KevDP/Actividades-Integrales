c324adb6f34b13549e4a18bb95dee470295ebeb0
KevDP <58478385+KevDP@users.noreply.github.com>
2020-12-01 23:21:24 -0600

    Update main.cpp

--- Actividad Integral 3. Árboles/main.cpp
+++ Actividad Integral 3. Árboles/main.cpp
@@ -3,56 +3,135 @@
 #include "heap.h"
 #include <fstream>
 #include <sstream>
+#include <vector>
+
 
 
 using namespace std;
 
-template <class T>
-bool leer(Heap<T> &mi_arbol, string nom_arch){
-  ifstream inFile(nom_arch);
-  if(!inFile){
-    cout<<"No es posible abrir el archivo"<<endl;
-    return false;
-  }
-  string val;
-  while(getline(inFile, val)){
-      if(val.size() > 0){
-        mi_arbol.push(val);
-		  }
-  }
-    cout<<"\n Datos obtenidos correctamente de archivo.txt\n"<<"\n";
+void leer();
+void escribir();
+void ordenar();
+void buscar();
+void desplegar();
+void casosPrueba();
 
-  inFile.close();
-  return true;
-}
+Heap<string> mi_arbol(100);
+vector<string> datos;
+string datcarta, filenameLoad;
 
 int main(){
 int opcion;
-Heap<string> mi_arbol;
-bool archivo = leer(mi_arbol, "datos.csv");
-string datcarta;
+
+cout<<"\n Ingresa el nombre del archivo a cargar: ";  
+cin>>filenameLoad;
+
+leer();
+
+cout<<"\n Datos obtenidos correctamente de: " << filenameLoad <<"\n"<< endl;
 
 do {
     cout << "Datos obtenidos del archivo"<<endl;
-		cout << "1. Desplegar datos\n";
-    cout << "2. Agregar dato\n";
-    cout << "3. Escribir datos en archivo\n";
-		cout << "4. Buscar dato\n";
+		cout << "1. Desplegar cartas\n";
+    cout << "2. Escribir datos de cartas en archivo\n";
+		cout << "3. Buscar carta\n";
+    cout << "4. Probar casos de prueba\n";
 		cout << "5. Exit\n";
 		cin >> opcion;
 		cin.ignore();
     switch (opcion){
-      case 1: mi_arbol.toString(); break;
-      case 2:  break;
-      case 3: mi_arbol.escribir("resultado.csv"); break;
-      case 4: cout<<"Introduce el dato a verificar si está en el archivo"<<endl;
-              cin>>datcarta;
-              mi_arbol.find(datcarta); break;
+      case 1: desplegar(); break;
+      case 2: escribir(); break;
+      case 3: buscar(); break;
+      case 4: casosPrueba(); break;
       case 5 : cout << "Saliendo..."; return 0;
 			default: cout << "Opcion incorrecta\n"; break;
 		}
 		cout << "\n\n";
-	} while (opcion != 5);
+	} while (opcion != 6);
 	return 0;
 }
 
+void leer(){
+ifstream inFile(filenameLoad);
+  string line = "";
+
+  while(getline(inFile, line)){
+    if(line.size() > 0){
+      mi_arbol.push(line);
+      datos.push_back(line);
+    }
+  }
+}
+
+void escribir(){
+  string filenameSave;
+
+  ofstream outFile;
+  cout << "\n Ingrese el nombre del archivo a guardar: \n"<<endl;
+  cin>>filenameSave;
+  outFile.open(filenameSave);
+  outFile << mi_arbol.toString() <<endl;
+  cout<<"\n Datos guardados correctamente en: " << filenameSave << endl;
+  outFile.close();
+}
+
+void buscar(){
+int cart;
+cout << "\n Ingrese el número de la carta a buscar: \n"<<endl;
+cin>>cart;
+for (int i = 0; i < mi_arbol.size(); i++) {
+  if(i == cart){
+   cout<<datos[i-1]<<endl;
+   if(cart == 50){
+     cout<<datos[50]<<endl;
+   }
+  }
+}
+cout<< "\n   Carta encontrada correctamente. \n"<<endl;
+}
+
+void desplegar(){
+  cout<<mi_arbol.toString();
+}
+
+void casosPrueba(){
+  Heap<int> heap(10);
+	string ans;
+	heap.push(99);
+  heap.push(9);
+
+  ans =	"[9 99]";
+	cout << " Prueba #1 " <<	(!ans.compare(heap.toStringCases()) ? "success\n" : "fail\n");
+
+	heap.push(56);
+	heap.push(7);
+	ans ="[7 9 56 99]";
+	cout << " Prueba #2 " <<	(!ans.compare(heap.toStringCases()) ? "success\n" : "fail\n");
+
+	heap.pop();
+  heap.push(1);
+	ans ="[1 9 56 99]";
+	cout << " Prueba #3 " <<	(!ans.compare(heap.toStringCases()) ? "success\n" : "fail\n");
+
+	cout << " Prueba #4 " <<	((4 == heap.size()) ? "success\n" : "fail\n");
+
+  heap.pop();
+  heap.pop();
+	ans ="[56 99]";
+	cout << " Prueba #5 " <<	(!ans.compare(heap.toStringCases()) ? "success\n" : "fail\n");
+
+	cout << " Prueba #6 " <<	((2 == heap.size()) ? "success\n" : "fail\n");
+
+	cout << " Prueba #7 " <<	((56 == heap.top()) ? "success\n" : "fail\n");
+
+	cout << " Prueba #8 " <<	((0 == heap.empty()) ? "success\n" : "fail\n");
+
+  heap.pop();
+
+	cout << " Prueba #9 " <<	((99 == heap.top()) ? "success\n" : "fail\n");
+
+	heap.pop();
+
+	cout << " Prueba #10 " <<	((1 == heap.empty()) ? "success\n" : "fail\n");
+}
