272fbb098c1d4e0e5ffdd63efae33b9343286d5e
KevDP <58478385+KevDP@users.noreply.github.com>
2020-12-03 10:53:59 -0600

    Update metodo.h

--- Actividad 1 - Conceptos basicos y algoritmos fundamentales/metodo.h
+++ Actividad 1 - Conceptos basicos y algoritmos fundamentales/metodo.h
@@ -10,77 +10,56 @@ using namespace std;
 class Metodo{
 
   private:
-    void leer();
-    void escribir();
-    void buscarxRangos();
-    void casosPrueba();
+    void leer();		// Leer archivo de texto
+    void escribir();		// Escribir en un nuevo archivo
+    void buscarxRangos();	// Buscar datos según un rango
 };
+
+vector<string> datos_txt;		// Utilizar un vector
+Sorts<string> sorts;		// Utilizar un sort
 
 
 void leer(){
-  vector<string> datos_txt;
+ 
   ifstream inFile;
   string line = "";
-  inFile.open("archivo.txt");
+  inFile.open("archivo.txt");		// Abrir el archivo
 
-  while(getline(inFile, line)){
-      if(line.size() > 0){
+  while(getline(inFile, line)){		// Leer cada dato por linea
+      if(line.size() > 0){		// Guardar linea hasta que no quede ninguna en el archivo
         datos_txt.push_back(line);
 		  }
   }
 
-     for(int i = 0; i < datos_txt.size(); i++){
+     for(int i = 0; i < datos_txt.size(); i++){		// Desplegar lo guardado en el vector
        cout<<datos_txt.at(i)<<"\n";
     }
     cout<<"\n Datos obtenidos correctamente de archivo.txt\n"<<"\n";
 
-  inFile.close();
+  inFile.close();		// Cerrar archivo
 
 }
 
 
 void escribir(){
-
-Sorts<string> sorts;
-vector<string> datos_txt;
-ifstream inFile;
-string line = "";
-inFile.open("archivo.txt");
 ofstream outFile;
 outFile.open("resultado.txt");
 
-  while(getline(inFile, line)){
-      if(line.size() > 0){
-        datos_txt.push_back(line);
-		  }
-    }
-
-  sorts.ordenaMerge(datos_txt);
+  sorts.ordenaMerge(datos_txt);	// Ordenar el vector mediante un merge sort
     
-  for (int i = 0; i < datos_txt.size(); i++) {
+  for (int i = 0; i < datos_txt.size(); i++) {		// Guardar cada uno de los datos del vector ordenando en archivo
     outFile << datos_txt[i] << "\n";
   }
     
-  for(int i = 0; i < datos_txt.size(); i++){
+  for(int i = 0; i < datos_txt.size(); i++){		// Desplegar datos del vector
     cout<<datos_txt.at(i)<<"\n";
   }
   cout<<"\n Datos ordenados correctamente y guardados en resultado.txt \n"<<"\n";
-  outFile.close();
+  outFile.close();					// Cerrar archivo
 }
 
 void buscarxRangos(){
-  vector<string> datos_txt;
-  ifstream inFile;
-  string line = "";
-  inFile.open("resultado.txt");
-  while(getline(inFile, line)){
-      if(line.size() > 0){
-        datos_txt.push_back(line);
-		  }
-  }
-
-  int min;
-  int max;
+  int min, max;
   cout<<"\n Escriba el rango de datos que quiere buscar.\n\n ";
   cout<<"\n Buscar de: \n";
   cin>>min;
